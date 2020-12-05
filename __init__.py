70569ed00219fa4bc545884ce9fd3a50a1c4622a
Jo Tjernshaugen <Xmann1>
2020-12-05 21:19:29 +0100

    Fixed key setup for GitHub

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
 import DASSBetjent
 import logging
 import yaml
-#  import os
+import os
 
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger("DASSBetjent").setLevel(logging.DEBUG)
 
-    with open("keys.yaml", "r") as fr:
-        keys = yaml.load(fr, Loader=yaml.FullLoader)
+    if os.path.exists("keys.yaml"):
+        with open("keys.yaml", "r") as fr:
+            keys = yaml.load(fr, Loader=yaml.FullLoader)
 
-    bot = DASSBetjent.DASSBetjent(prefix="+")
-    bot.run(keys["npst"], keys["discord"])
+        bot = DASSBetjent.DASSBetjent(prefix="+")
+        bot.run(keys["npst"], keys["discord"])
+    else:
+        with open("keys.yaml", "w") as fw:
+            yaml.dump({"npst": "ENTER NPST TOKEN HERE", "discord": "ENTER DISCORD TOKEN HERE"}, fw)
+        print(f"Please enter keys in keys.yaml")
