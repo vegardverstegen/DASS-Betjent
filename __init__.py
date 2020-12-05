663d4822a3a009031fca4363542cab2a8a93e49d
Jo Tjernshaugen <Xmann1>
2020-12-05 21:25:56 +0100

    Fixed authentication problem

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             keys = yaml.load(fr, Loader=yaml.FullLoader)
 
         bot = DASSBetjent.DASSBetjent(prefix="+")
-        bot.run(keys["npst"], keys["discord"])
+        bot.run(keys["discord"], keys["npst"])
     else:
         with open("keys.yaml", "w") as fw:
             yaml.dump({"npst": "ENTER NPST TOKEN HERE", "discord": "ENTER DISCORD TOKEN HERE"}, fw)
